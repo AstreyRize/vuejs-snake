b42c0997eff33ef01507ed3c38400606a3c8db4c
Degtyarev Petr <petr.degtyarev@norbit.ru>
2019-07-28 22:51:18 +0300

    Теперь змейка движется

--- js/snake.js
+++ js/snake.js
@@ -2,7 +2,12 @@ Vue.component('snake', {
     data: function () {
         return {
             showComponent: true,
-            shakeLength: 5,
+            direction: 'left',
+            startTimeInterval: 120,
+            snake: {
+                shakeLength: 5,
+                body: [this.shakeLength]
+            },
             field: {
                 fieldRows: 50,
                 fieldCells: 50,
@@ -26,8 +31,12 @@ Vue.component('snake', {
             let startRow = this.field.fieldRows / 2;
             let startCell = this.field.fieldCells / 2;
 
-            for (let snakeBody = 0; snakeBody < this.shakeLength; snakeBody++) {
-                this.field.cells[startRow][startCell + snakeBody].status = 'snake';
+            for (let snakeBody = 0; snakeBody < this.snake.shakeLength; snakeBody++) {
+                // Координаты каждой секции змейки.
+                this.snake.body[snakeBody] = {
+                    row: startRow,
+                    cell: startCell + snakeBody
+                };
             }
         },
         initDot: function () {
@@ -42,18 +51,80 @@ Vue.component('snake', {
                 let minCell = 0;
                 let maxCell = this.field.fieldRows;
                 cell = Math.floor(Math.random() * (+maxCell - +minCell)) + +minCell;
-            } while (this.field.cells[row][cell].status == 'snake');
+            } while (this.field.cells[row][cell].status === 'snake');
 
             this.field.cells[row][cell].status = 'dot';
         },
-        start: function (event) {
+        startGame: function (event) {
+            setInterval(this.tick, this.startTimeInterval);
+        },
+        tick: function () {
             debugger;
+
+            // Пересчитываем координаты тела змейки.
+            let newBody = [this.snake.shakeLength];
+
+            // На основенаправления задаем новые координаты головы
+            switch (this.direction) {
+                case 'top':
+                    newBody[0] = {
+                        row: this.snake.body[0].row - 1,
+                        cell: this.snake.body[0].cell,
+                    };
+                    break;
+                case 'bottom':
+                    newBody[0] = {
+                        row: this.snake.body[0].row + 1,
+                        cell: this.snake.body[0].cell,
+                    };
+                    break;
+                case 'left':
+                    newBody[0] = {
+                        row: this.snake.body[0].row,
+                        cell: this.snake.body[0].cell - 1,
+                    };
+                    break;
+                case 'right':
+                    newBody[0] = {
+                        row: this.snake.body[0].row,
+                        cell: this.snake.body[0].cell + 1,
+                    };
+                    break;
+            }
+
+            for (let snakeBody = 1; snakeBody < this.snake.body.length; snakeBody++) {
+                newBody[snakeBody] = {
+                    row: this.snake.body[snakeBody - 1].row,
+                    cell: this.snake.body[snakeBody - 1].cell
+                }
+            }
+
+            this.renderField(newBody);
+        },
+        renderField: function (newBody) {
+            let oldCellRow = this.snake.body[this.snake.shakeLength - 1].row;
+            let oldCell = this.snake.body[this.snake.shakeLength - 1].cell;
+
+            this.setCell(oldCellRow, oldCell, 'none');
+            this.snake.body = newBody;
+
+            for (let snakeBody = 0; snakeBody < this.snake.shakeLength; snakeBody++) {
+                let row = this.snake.body[snakeBody].row;
+                let cell = this.snake.body[snakeBody].cell;
+
+                this.setCell(row, cell, 'snake');
+            }
+        },
+        setCell: function (row, cell, status) {
+            let changedRow = this.field.cells[row];
+            Vue.set(changedRow, cell, {status: status});
+            Vue.set(this.field.cells, row, changedRow);
         }
     },
     beforeMount() {
         this.initField();
         this.initSnake();
         this.initDot();
+        this.startGame();
     }
-})
-;
+});
